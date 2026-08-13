9ef7e8ef2a3ecacb1b79f0f158f78b9d22333fad
Varun-1212 <varunnomula01@gmail.com>
2026-08-13 17:27:36 +0530

    Update FirstTest.java

--- src/test/java/example/FirstTest.java
+++ src/test/java/example/FirstTest.java
@@ -31,8 +31,7 @@ public class FirstTest {
             else {
                 System.out.println("The Title's are diffferent");
             }
-        }
-        catch (Exception e)
+        }catch (Exception e)
         {
             e.printStackTrace();
         }
